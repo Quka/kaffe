cf816310272a79f407d47e56fb8b89e799adf0d3
Arlind Ukshini <quka93@gmail.com>
2018-09-05 13:39:23 +0200

    add FlatWhite and price

---
+++ kaffe/FlatWhite.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kaffe
+{
+	class FlatWhite : Kaffe, Imælk
+	{
+		public int MlMælk()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override int Pris()
+		{
+			return 45;
+		}
+	}
+}
